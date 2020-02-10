d17154e3ad162583e8a9cd4412fa04fb0f789c55
michalszmigiel <michal.r.szmigiel@gmail.com>
2020-02-10 20:50:11 +0100

    displaying results

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ if(!$db_connection)
     echo "Connection error: " .mysqli_connect_error();
 }
 
-$sql = "select title, ingredients, id from pizzas";
+$sql = "SELECT title, ingredients, id FROM pizzas ORDER BY created_at";
 
 $result = mysqli_query($db_connection, $sql);
 
@@ -20,8 +20,25 @@ mysqli_close($db_connection);
 <?php
 require 'templates/header.php'; ?>
 
+<h4 class="center grey-text">PIZZAS :)</h4>
 
-
+    <div class="container">
+        <div class="row">
+            <?php foreach ($pizzas as $pizza): ?>
+                <div class="col s6 md3">
+                    <div class="card z-depth-0">
+                        <div class="card-content center">
+                            <h6><?=htmlspecialchars($pizza['title']);?></h6>
+                            <div><?=htmlspecialchars($pizza['ingredients']);?></div>
+                        </div>
+                        <div class="card-action right-align">
+                            <a href="/" class="brand-text">More info</a>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
 
 <?php
 require 'templates/footer.php'; ?>
