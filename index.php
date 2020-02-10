78e1616b828c0a4d1eba4f358144b26b5fc2a35c
michalszmigiel <michal.r.szmigiel@gmail.com>
2020-02-10 20:41:26 +0100

    db connection, retrieving results

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
 <?php
+$db_connection = mysqli_connect('localhost','root','','pizza_db');
+if(!$db_connection)
+{
+    echo "Connection error: " .mysqli_connect_error();
+}
+
+$sql = "select title, ingredients, id from pizzas";
+
+$result = mysqli_query($db_connection, $sql);
+
+$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+mysqli_free_result($result);
+
+mysqli_close($db_connection);
+
+;?>
+
+<?php
 require 'templates/header.php'; ?>
+
+
+
 
 <?php
 require 'templates/footer.php'; ?>
