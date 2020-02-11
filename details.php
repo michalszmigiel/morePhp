295d36a3e7509750c0bda06cf523f4e3ad559234
michalszmigiel <michal.r.szmigiel@gmail.com>
2020-02-11 10:16:19 +0100

    deleting by id

--- details.php
+++ details.php
@@ -2,6 +2,22 @@
 
 require 'config/db_connection.php';
 
+if(isset($_POST['delete']))
+{
+    $id_to_delete = mysqli_real_escape_string($db_connection, $_POST['id_to_delete']);
+
+    $sql = "DELETE FROM pizzas WHERE id = $id_to_delete";
+
+    if(mysqli_query($db_connection, $sql))
+    {
+        header('Location: /');
+    }
+    else
+    {
+        echo "Query error: " .mysqli_error($db_connection);
+    }
+}
+
 if (isset($_GET['id']))
 {
     $id = mysqli_real_escape_string($db_connection, $_GET['id']);
@@ -28,6 +44,11 @@ require 'templates/header.php'; ?>
     <p><?= date($pizza['created_at']);?></p>
     <h5>Ingredients:</h5>
     <p><?= htmlspecialchars($pizza['ingredients']);?></p>
+
+    <form action="details.php" method="post">
+        <input type="hidden" name="id_to_delete" value="<?= $pizza['id'];?>"/>
+        <input type="submit" name="delete" value="Delete pizza" class="btn brand z-depth-o">
+    </form>
 
     <?php else: ?>
     <h5>No such pizza exists. :(</h5>
